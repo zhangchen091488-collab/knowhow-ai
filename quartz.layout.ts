583122e352457c668a71f139a610f35878218263
晨老师 <zhangchen091488@gmail.com>
2026-03-14 19:54:00 +0800

    feat: 添加文件夹索引和 Explorer 配置

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -38,7 +38,10 @@ export const defaultContentPageLayout: PageLayout = {
         { Component: Component.ReaderMode() },
       ],
     }),
-    Component.Explorer(),
+    Component.Explorer({
+      folderClickBehavior: "link",
+      useSavedState: true,
+    }),
   ],
   right: [
     Component.Graph(),
