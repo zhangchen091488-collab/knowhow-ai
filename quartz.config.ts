ed3cbe03f3454472f8d64fcdab378745bb5aa025
晨老师 <zhangchen091488@gmail.com>
2026-03-14 19:43:24 +0800

    feat: 添加中文首页和配置优化

--- quartz.config.ts
+++ quartz.config.ts
@@ -15,8 +15,8 @@ const config: QuartzConfig = {
     analytics: {
       provider: "plausible",
     },
-    locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    locale: "zh-CN",
+    baseUrl: "knowhow-ai.vercel.app",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
